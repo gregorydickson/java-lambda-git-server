a661ad096b672ef83c42ce863401146f0045ef2a
Gregg Dickson <gregory.d.dickson@gmail.com>
2016-04-26 09:44:36 -0500

    in progress

--- src/main/java/gitServeLambda.java
+++ src/main/java/gitServeLambda.java
@@ -14,6 +14,9 @@ import org.eclipse.jgit.transport.FetchResult;
 import org.rauschig.jarchivelib.Archiver;
 import org.rauschig.jarchivelib.ArchiverFactory;
 
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.Context; 
+
 import com.google.common.io.Files;
 import com.restfb.json.JsonObject;
 
@@ -26,7 +29,7 @@ public class GitServeLambda implements RequestHandler<LambdaHttpServletRequest, 
         LambdaHttpServletResponse response = new LambdaHttpServletResponse();
         
         //get uri for file
-
+        String urlPath = request.getRequestUri();
         // build file
 
         //serve file
@@ -39,8 +42,7 @@ public class GitServeLambda implements RequestHandler<LambdaHttpServletRequest, 
 
 
     public static void main(String[] args) throws InvalidRemoteException, 
-                          TransportException, GitAPIException, IOException,
-                           {
+                          TransportException, GitAPIException, IOException  {
 
         
 
@@ -62,9 +64,7 @@ public class GitServeLambda implements RequestHandler<LambdaHttpServletRequest, 
             gruntBuild(sourceRepo);
             System.out.println(sourceRepo);
 
-            String version = "0.0."+new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-            packageAndDeploy(new File(new File(new File(sourceRepo, "build"), "packages"), "react.tgz"), version);
-            packageAndDeploy(new File(new File(new File(sourceRepo, "build"), "packages"), "react-dom.tgz"), version);
+            
         }
         finally
         {
@@ -75,47 +75,7 @@ public class GitServeLambda implements RequestHandler<LambdaHttpServletRequest, 
 
     }
 
-    public static void packageAndDeploy(File tarball, String version) throws IOException
-    {
-        File temp = Files.createTempDir();
-        Archiver archiver = ArchiverFactory.createArchiver("tar", "gz");
-        archiver.extract(tarball, temp);
-        System.out.println(temp);
-        File packageJson = new File(new File(temp, "package"), "package.json");
-        JsonObject json = new JsonObject(FileUtils.readFileToString(packageJson));
-        json.put("name", json.get("name")+"-future");
-        json.put("version", version);
-        json.put("description", "A future/experimental build of React with features and functionality being added/considered for a future version of React.  This build follows master, which means it is up-to-date and passes unit tests but is not as stable as a stable release."); //   Used for integration testing, research, and development.
-
-        // Currently not setting the dependency on react-future (leaving it depend on React) since react-future isn't strictly necessary and we want to maintain compatibility
-/*      if(!"react-future".equals(json.get("name")))
-        {
-            JsonObject dependencies = new JsonObject();
-            dependencies.put("react-future", "^0.0."+version);
-            json.put("dependencies", dependencies);
-        }
-*/      if("react-future".equals(json.get("name")))
-        {
-            File readmeFile = new File(new File(temp, "package"), "README.md");
-            String readme = FileUtils.readFileToString(readmeFile);
-            readme = readme.replace("npm install react", "npm install react-future");
-            FileUtils.writeStringToFile(readmeFile, readme);
-        }
-        if("react-dom-future".equals(json.get("name")))
-        {
-            File readmeFile = new File(new File(temp, "package"), "README.md");
-            String readme = FileUtils.readFileToString(readmeFile);
-            //readme = readme.replace("require('react')", "require('react-future')");
-            readme = readme.replace("react-dom", "react-dom-future");
-            FileUtils.writeStringToFile(readmeFile, readme);
-
-            json.getJsonObject("peerDependencies").remove("react");
-            json.getJsonObject("peerDependencies").put("react-future", version);
-        }
-        FileUtils.writeStringToFile(packageJson, json.toString());
-        npmPublish(new File(temp, "package"));
-        FileUtils.deleteDirectory(temp);
-    }
+ 
 
     public static void npmPublish(File directory) throws IOException
     {
